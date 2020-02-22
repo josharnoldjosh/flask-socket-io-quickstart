21d393eba0c28a3e073b8da57d558679344736c9
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-02-22 06:39:05 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
+
+# Init the server
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, logger=True)
+
+@app.route('/')
+def root():
+    """ Send HTML from the server."""
+    return render_template('index.html')
+
+@app.route('/<email>/<game_id>/<user_type>')
+def game(email, game_id, user_type):
+    if user_type == "drawer":
+        return render_template('drawer.html', game_id=game_id)
+    else:
+        return render_template('teller.html', game_id=game_id)
+
+@socketio.on('message')   
+def message_recieved(message):    
+    emit('display_message', message, broadcast=True)
+    
+
+if __name__ == '__main__':
+    """ Run the app. """
+    socketio.run(app, port=2000)
